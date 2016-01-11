2b6fde22f8b11958530af499953339968ecd75a8
David Deller <david.deller@tripcraft.com>
2016-01-11 12:38:15 -0500

    Create project

---
+++ Converge/Converge.h
@@ -0,0 +1,19 @@
+//
+//  Converge.h
+//  Converge
+//
+//  Created by David (work) on 1/11/16.
+//  Copyright © 2016 TripCraft LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Converge.
+FOUNDATION_EXPORT double ConvergeVersionNumber;
+
+//! Project version string for Converge.
+FOUNDATION_EXPORT const unsigned char ConvergeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Converge/PublicHeader.h>
+
+
