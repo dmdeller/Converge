73906d1094123eaf9c63ad55443f415de905d46a
David Deller <david.deller@tripcraft.com>
2016-01-12 15:57:40 -0500

    Add TransformerKit and InflectorKit via CocoaPods

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+pod 'TransformerKit', '~> 0.5'
+pod 'InflectorKit', '~> 0.0'
+
+#target 'Converge' do
+#
+#end
+#
+#target 'ConvergeTests' do
+#
+#end
+
---
+++ Pods/Target Support Files/TransformerKit/TransformerKit-umbrella.h
@@ -0,0 +1,12 @@
+#import <UIKit/UIKit.h>
+
+#import "NSValueTransformer+TransformerKit.h"
+#import "TTTCryptographyTransformers.h"
+#import "TTTDateTransformers.h"
+#import "TTTImageTransformers.h"
+#import "TTTJSONTransformer.h"
+#import "TTTStringTransformers.h"
+
+FOUNDATION_EXPORT double TransformerKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char TransformerKitVersionString[];
+
